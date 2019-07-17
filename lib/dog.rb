e8b41ce66500b49044725d31dd1eb11d9334df55
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-17 04:35:45 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+    def name=(dog_name)
+      @name = dog_name
+     
+    end 
+    
+    def name 
+      @name
+    end
+  end 
+  
+  
+  fido = Dog.new
+  fido.name = "Fido"
+   
+  puts fido.name
+
